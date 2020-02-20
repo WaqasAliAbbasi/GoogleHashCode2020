ffe347dc67aeff3f9c09c8bcc5a276b6f1d6545b
utkarsh867 <utkarsh867@gmail.com>
2020-02-21 01:47:36 +0530

    tweaks the setSelfWorth

--- library.py
+++ library.py
@@ -5,17 +5,25 @@ class Library:
         self.signUpTime = signUpTime
         self.capacityOfShipping = capacityOfShipping
         self.books = books
+        self.isSignedUp = False
         self.worth = self.setSelfWorth(days)
 
     def __lt__(self, other):
         return self.worth > other.worth
 
     def setSelfWorth(self, days):
-        shippingDays = days - self.signUpTime
+        if self.isSignedUp:
+            shippingDays = days
+        else:
+            shippingDays = days - self.signUpTime
+
         noOfBooksThatCanBeScanned = self.capacityOfShipping*shippingDays
         booksToBeScanned = self.books[:noOfBooksThatCanBeScanned]
         worth = sum([book.bookWorth for book in booksToBeScanned])
         return worth
+
+    def signUpLibrary(self):
+        self.isSignedUp = True
 
     def __repr__(self):
         return "(ID: {}, Number of Books: {}, Sign Up Time: {}, Capacity of Shipping: {}, Total Books: {})".format(self.libraryID, self.numberOfBooks, self.signUpTime, self.capacityOfShipping, len(self.books))
